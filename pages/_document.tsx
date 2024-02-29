009d5583aa7636e348a11bcaa38d95a379d1f8af
vinniedaboi <kidosol5224@gmail.com>
2024-02-29 19:07:33 +0800

    PWA support for mobile.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,13 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<ColorSchemeScript defaultColorScheme="dark" localStorageKey="mantine-ui-color-scheme" />
+				<link rel='manifest' href='/manifest.json' />
+				<link rel='apple-touch-icon' href='/icon-512x512.png' />
+				<meta name='theme-color' content='#000' />
+				<ColorSchemeScript
+					defaultColorScheme='dark'
+					localStorageKey='mantine-ui-color-scheme'
+				/>
 			</Head>
 			<body>
 				<Main />
