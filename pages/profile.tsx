df7fe547bb4ba8f080f01d758d8774ac5ad6966e
vinniedaboi <kidosol5224@gmail.com>
2024-04-14 19:59:03 +0800

    Added details to profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,33 +2,30 @@ import Page from '@/components/page'
 import Section from '@/components/section'
 import { useUser } from '@clerk/nextjs'
 import { OrganizationSwitcher, SignedIn, UserButton } from '@clerk/nextjs'
+import { UserProfile } from '@clerk/nextjs'
+import { useDisclosure } from '@mantine/hooks'
+import { Box, Group, Button, Collapse, Text} from '@mantine/core'
+
+// currently working on getting database up and running, as well as managing organizations/invites and user profiles and data.
 
 export default function Profile() {
-	const { isLoaded, user } = useUser()
+	const { isLoaded, user } = useUser();
+	const [opened, { toggleAccountSettings }] = useDisclosure(false);
 
 	return (
 		<Page>
 			{isLoaded && user ? (
 				<>
-					<div className='pb-6 max-h-96 flex space-x-6'>
-						<dl>
-							<div className='px-8 py-2'>
-								<dt className='text-sm font-semibold'>User ID</dt>
-								<dd className='mt-1 text-sm text-white sm:mt-0 sm:col-span-2 flex gap-2'>
-									{user.id}
-								</dd>
-							</div>
-							{user.firstName && (
-								<div className='px-8 py-2'>
-									<dd className='mt-1 text-sm text-white sm:mt-0 sm:col-span-2'>
-										{user.firstName} {user.lastName}
-									</dd>
-								</div>
-							)}
-						</dl>
-					</div>
+				<Group>
 					<div>
 						<OrganizationSwitcher afterCreateOrganizationUrl='/profile' />
+					</div>
+				</Group>
+					<div className='pb-6 max-h-96 flex space-x-6'>
+
+					</div>
+					<div>
+						<UserProfile></UserProfile>
 					</div>
 				</>
 			) : (
@@ -39,3 +36,4 @@ export default function Profile() {
 		</Page>
 	)
 }
+
